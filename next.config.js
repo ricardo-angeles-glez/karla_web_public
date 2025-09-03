dc3c9dcf1f44c1f8c1ed160d1a55cc2f2239632a
ricardo-angeles-glez <55196878+ricardo-angeles-glez@users.noreply.github.com>
2025-09-03 02:07:20 +0000

    Subimos todo public

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // Genera sitio estático listo para GitHub Pages
+  output: 'export', // Genera sitio estático
+  basePath: '/karla_web_public', // TU REPO
+  assetPrefix: '/karla_web_public/', // Para CSS, JS y assets
   images: {
-    unoptimized: true, // Evita errores si usas next/image
+    unoptimized: true, // Si usas next/image
   },
-  basePath: '/karla_web_public', // Tu repo de GitHub Pages
-  assetPrefix: '/karla_web_public/', // Para assets como imágenes y audio
 };
 
 module.exports = nextConfig;
