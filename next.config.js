af8711e2d755c72eaeb1b51f3a1d825e35a12c91
ricardo-angeles-glez <55196878+ricardo-angeles-glez@users.noreply.github.com>
2025-09-03 00:40:39 +0000

    Fix deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',             // Indica que vamos a exportar a HTML estático
+  basePath: '/karla_web_public', // Debe coincidir con el nombre del repo
+
   webpack: (config, { dev, isServer }) => {
     if (dev && !isServer) {
       config.cache = {
